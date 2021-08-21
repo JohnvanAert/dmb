cca860809ade465e7e7f5b8c3f613398a95d4711
John <77893046+JohnvanAert@users.noreply.github.com>
2021-08-21 18:39:56 +0600

    added id to voice_chnnel

--- commands/play.js
+++ commands/play.js
@@ -9,7 +9,7 @@ module.exports = {
     cooldown: 1,
     description: 'Музыкальный Бот',
     async execute(message,args, cmd, client, Discord){
-        const VCchannelID = ('id');
+        const VCchannelID = ('856860838324207656');
         const voice_channel = guild.channels.cache.get(VCchannelID);
         if (!voice_channel)return message.channel.send('Тебе нужно быть на голосовом канале!');
         const permissions = voice_channel.permissionsFor(message.client.user);
